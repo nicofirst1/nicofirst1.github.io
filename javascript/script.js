464bb49f1187ca0141336ecf8979f057f1daae30
dizzi <nicofirst1@gmail.com>
2020-10-07 18:15:37 +0200

    implemented multi-html

--- javascript/script.js
+++ javascript/script.js
@@ -6,11 +6,32 @@
 
 
 
-  $(function(){
-    $("#home-placeholder").load("html/home.html");
-  });
+  $(window).on('hashchange', function () {
+
+    let hash=window.location.hash
+    if (hash == '#projects') {
+        $("#placeholder").load("html/projects.html");
+    }
+
+    else if(hash == "#publications"){
+        $("#placeholder").load("html/publications.html");
+
+    }
+
+    else if(hash == "#cv"){
+        $("#placeholder").load("html/cv.html");
+
+    }
+
+    else if(hash == "#about"){
+        $("#placeholder").load("html/about.html");
+
+    }
 
 
-  $(function(){
-    $("#head-placeholder").load("html/head.html");
-  });
+    else{
+        $("#placeholder").load("html/home.html");
+    }
+
+
+});
