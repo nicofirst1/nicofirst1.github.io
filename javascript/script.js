9e7e9552fa0186f8aa3806dc3feda5bf1e1c97e8
dizzi <nicofirst1@gmail.com>
2020-10-07 17:51:31 +0200

    split home and nav

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+
+
+  $(function(){
+    $("#nav-placeholder").load("html/navbar.html");
+  });
+
+
+
+  $(function(){
+    $("#home-placeholder").load("html/home.html");
+  });
+
+
+  $(function(){
+    $("#head-placeholder").load("html/head.html");
+  });
