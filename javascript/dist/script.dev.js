0e43bda3a8663019c4e5a2b7bf716e22da360b8c
Nicolo <nicofirst1@gmail.com>
2021-01-12 19:55:17 +0100

    done

--- javascript/dist/script.dev.js
+++ javascript/dist/script.dev.js
@@ -1,65 +1,53 @@
+"use strict";
+
 // Download CV
 function download_cv2() {
-  const filename = 'NicoloBrandizziCV.pdf';
-
+  var filename = 'NicoloBrandizziCV.pdf';
   html2canvas(document.getElementById('cv_container'), {
     scale: 1
-  }).then(canvas => {
-    let pdf = new jsPDF('p', 'mm', 'a4');
-    pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 210, 297);
+  }).then(function (canvas) {
+    var pdf = new jsPDF('p', 'mm', 'a4');
+    pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
     pdf.save(filename);
   });
-
 }
 
-
-
 function download_cv() {
-  const filename = 'NicoloBrandizziCV.pdf';
-
+  var filename = 'NicoloBrandizziCV.pdf';
   var deferreds = [];
-  let doc = new jsPDF('p', 'mm', 'a4');
-  let container = document.getElementById('cv_container');
-  var children = Array.from(container.children);
-  console.log(children)
+  var doc = new jsPDF('p', 'mm', 'a4');
+  var container = document.getElementById('cv_container');
+  var children = container.children;
+  console.log(children.length);
+
   for (var i = 0; i < children.length; i++) {
     var deferred = $.Deferred();
     deferreds.push(deferred.promise());
     generateCanvas(children[i], doc, deferred);
   }
 
-  $.when.apply($, deferreds).then(function () { // executes after adding all images
+  $.when.apply($, deferreds).then(function () {
+    // executes after adding all images
     doc.save(filename);
   });
-
-  function generateCanvas(elem, doc, deferred) {
-
-    html2canvas(elem, {
-      scale: 4,
-      onrendered: function (canvas) {
-
-        var img = canvas.toDataURL();
-        doc.addImage(img, 'JPEG', 0, 0, 210, 297);
-        doc.addPage();
-
-        deferred.resolve();
-      }
-    });
-  }
-
 }
 
+function generateCanvas(elem, doc, deferred) {
+  html2canvas(elem, {
+    onrendered: function onrendered(canvas) {
+      var img = canvas.toDataURL();
+      doc.addImage(img, 'PNG', 0, 0, 211, 298);
+      doc.addPage();
+      deferred.resolve();
+    }
+  });
+} // Load navbar
 
-
-
-// Load navbar
 
 $(function () {
   $("#nav-placeholder").load("html/navbar.html");
+}); // disable resizing for cv
 
-});
-
-// disable resizing for cv
 function no_resize() {
   var screenWidth = $(window).width();
   $('container').css('width', 1200 + 'px');
@@ -67,18 +55,18 @@ function no_resize() {
 }
 
 function init_colors() {
-  console.log(document.getElementById("dark_color"))
-  document.getElementById("dark_color").value = "#042F33";
-  document.getElementById("light_color").value = "#06727C";
-  document.getElementById("gray_color").value = "#CBCDCD";
-  document.getElementById("contrast_color").value = "#53A0A7";
+  console.log(document.getElementById("dark_color"));
+  document.getElementById("dark_color").value = "#2c67fc";
+  document.getElementById("light_color").value = "#e2f7ff";
+  document.getElementById("gray_color").value = "#bbc6f0";
+  document.getElementById("contrast_color").value = "#7f45eb";
+} // modify cv colors
 
-}
 
-// modify cv colors
 function modify_color(elem) {
-  console.log(elem)
-  let id = elem.id;
+  console.log(elem);
+  var id = elem.id;
+
   if (id.includes("light")) {
     less.modifyVars({
       light: elem.value
@@ -96,86 +84,65 @@ function modify_color(elem) {
       other: elem.value
     });
   }
+} // Load home on first load instance
 
-}
-
-// Load home on first load instance
 
 function load() {
-  $("#placeholder").load("html/home.html");
-  window.location.hash = "#home"
+  $("#placeholder").load("html/cv/main.html");
+  window.location.hash = "#home";
+} // load cv parts
 
-}
 
-// load cv parts
 function load_cv() {
-  $("#hl1-placeholder").load("../html/cv/highlights/hl1.html");
-  $("#hl2-placeholder").load("../html/cv/highlights/hl2.html");
+  $("#highlights-placeholder").load("../html/cv/highlights.html");
   $("#education-placeholder").load("../html/cv/sections/education.html");
   $("#research-int-placeholder").load("../html/cv/sections/research_int.html");
   $("#publication-placeholder").load("../html/cv/sections/publications.html");
   $("#experiences-placeholder").load("../html/cv/sections/experiences.html");
   $("#headline-placeholder").load("../html/cv/headline.html");
+} // Function to load correct html based on hash
 
-}
-
-// Function to load correct html based on hash
 
 $(window).on('hashchange', function () {
+  var hash = window.location.hash;
 
-  let hash = window.location.hash
   if (hash == '#projects') {
     $("#placeholder").load("html/projects/main.html");
   } else if (hash == "#publications") {
     $("#placeholder").load("html/publications.html");
-
   } else if (hash == "#cv") {
     $("#placeholder").load("html/cv/main.html");
-
   } else if (hash == "#about") {
     $("#placeholder").load("html/about.html");
-
   } else {
     $("#placeholder").load("html/home.html");
-  }
+  } // Activate the correct navbar button
 
-  // Activate the correct navbar button
+
   $('.navbar').find('.active').removeClass('active');
   $('.navbar a').each(function () {
     if (this.href.indexOf(hash) > -1) {
       $(this).addClass('active');
     }
   });
+}); // Load project description based on hover
 
-
-
-});
-
-// Load project description based on hover
 function show_project(prj) {
-
-  let id = prj.id
+  var id = prj.id;
 
   if (id == "ledypi") {
     $("#project-placeholder").load("../html/projects/ledypi.html");
-
   } else if (id == "robotic") {
     $("#project-placeholder").load("../html/projects/robotic.html");
-
   } else if (id == "motionbot") {
     $("#project-placeholder").load("../html/projects/motionbot.html");
-
   } else if (id == "mas-traffic") {
     $("#project-placeholder").load("../html/projects/mas_traffic.html");
-
   } else if (id == "ai-notes") {
     $("#project-placeholder").load("../html/projects/ai_notes.html");
-
   } else if (id == "rl-werewolf") {
     $("#project-placeholder").load("../html/projects/rl_werewolf.html");
-
   } else if (id == "egg") {
     $("#project-placeholder").load("../html/projects/egg.html");
-
   }
 }
